43da1d046088aad6c741c918fc2a5b78608f4472
Zander Ellis <zander.ellis@wearephlo.com>
2022-10-09 20:53:36 +0100

    tutorial completed up to 07:27

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { About } from "./About";
+import { Book } from "./Book";
+import { BookList } from "./BookList";
+import { Contact } from "./Contact";
+import { Home } from "./Home";
+import { NewBook } from "./NewBook";
+import { NotFound } from "./NotFound";
+
+export { About, Book, BookList, Contact, Home, NewBook, NotFound };
